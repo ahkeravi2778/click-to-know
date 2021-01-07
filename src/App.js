0c369a0f8c1e6a0590625b078f852979683195ae
Ravi <ravi.exe.doc@gmail.com>
2021-01-07 18:40:39 +0530

    second

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Ahkeravi Click To Know More
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
