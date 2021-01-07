6b452bc2daaa9bfd85d105cb519700e5764b23bd
Ravi <ravi.exe.doc@gmail.com>
2021-01-07 18:41:57 +0530

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Ahkeravi Click To Know More
+         Ahkeravi Click To Know More...
         </p>
         <a
           className="App-link"
